fa8643df8d13c37669123bc5c0b89bda8f0888f7
alexander <alexander.zayats@gmail.com>
2016-11-17 16:59:58 +0200

    Save player.currentTime

--- video_xblock/static/js/player_state.js
+++ video_xblock/static/js/player_state.js
@@ -6,26 +6,33 @@ var player_state = {
   'volume': {{ player_state.volume }},
   'currentTime': {{ player_state.current_time }},
   'playbackRate': {{ player_state.playback_rate }},
-  'muted': {{ player_state.muted | yesno:"true,false" }}
+  'muted': {{ player_state.muted | yesno:"true,false" }},
+};
+
+var setInitialState = function(player, state) {
+	if (state.currentTime > 0) {
+		player.currentTime(state.currentTime);
+	};
+	player
+	  .volume(state.volume)
+	  .muted(state.muted)
+	  .playbackRate(state.playbackRate);
 };
 
 var saveState = function(){
   var player = this;
-  console.log('Save state');
   var new_state = {
     'volume': player.volume(),
-    'currentTime': player.ended()? 0 : player.currentTime(),
+    'currentTime': player.ended()? 0 : Math.floor(player.currentTime()),
     'playbackRate': player.playbackRate(),
-    'muted': player.muted()
+    'muted': player.muted(),
   };
+
   if (JSON.stringify(new_state) !== JSON.stringify(player_state)) {
+	  console.log('Saving state');
     player_state = new_state;
     parent.postMessage({'action': 'save_state', 'state': new_state}, document.origin)
   };
-};
-
-var saveCurrentTime = function() {
-
 };
 
 domReady(function() {
@@ -33,13 +40,13 @@ domReady(function() {
     var player = this;
 
     // Restore default or previously saved player state
-    player.volume({{ player_state.volume }});
-    player.muted({{ player_state.muted | yesno:"true,false" }});
-    player.playbackRate({{ player_state.playback_rate }})
+    setInitialState(player, player_state);
 
     player
       .on('volumechange', saveState)
-      .on('ratechange', saveState);
-      // .on('timeupdate', saveState);
+      .on('ratechange', saveState)
+      .on('play', saveState)
+      .on('pause', saveState)
+      .on('ended', saveState);
   });
 });
