dcc9634d762297088914de8605188ac8ca1d4e9d
Alexander Marenich <sendr84@gmail.com>
2017-01-20 14:32:06 +0200

    remarks correction #12

--- video_xblock/static/js/player-context-menu.js
+++ video_xblock/static/js/player-context-menu.js
@@ -65,11 +65,7 @@ domReady(function() {
         // Wrap into conditional statement to avoid unnecessary variables initialization
         if (menuItemClicked && noSubmenuClicked) {
             var labelLength = labelElement.length;
-            /*eslint-disable*/
-
-            const lineFeedCode = 10;
-
-            /*eslint-enable*/
+            const lineFeedCode = 10;  /* eslint-env es6 */
             // Check if the last character is an escaped one (line feed to get rid of) which is the case for Microsoft Edge
             if (labelElement.charCodeAt(labelLength-1) === lineFeedCode) {
                 var labelElementSliced = labelElement.slice(0, -1);
