9b7344f0298c21a1a731db40fc240f92e2d74d62
TamoshaytisV <amd.vladas@gmail.com>
2017-02-08 18:35:19 +0000

    added base exception

---
+++ video_xblock/exceptions.py
@@ -0,0 +1,21 @@
+"""
+Custom, VideoXblock specific Exceptions
+"""
+from __future__ import unicode_literals
+
+from video_xblock.constants import status
+from video_xblock.utils import ugettext as _
+
+
+class VideoXBlockException(Exception):
+    """
+    Base class for video xblock exceptions.
+    Subclasses should provide `.default_detail` property.
+    """
+    default_detail = _('An exception occurred.')
+
+    def __init__(self, detail=None):
+        self.detail = detail if detail is not None else self.default_detail
+
+    def __str__(self):
+        return self.detail
