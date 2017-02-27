0567c9190849208c81b777f9c31ae56331f0171a
xahgmah <xahgmah@yandex.ru>
2017-02-27 15:58:04 +0200

    Fix one second cycling
    Fix offset saving
    Fix `domReady`s visible

--- video_xblock/static/js/context.js
+++ video_xblock/static/js/context.js
@@ -1,5 +1,16 @@
+/** Run a callback when DOM is fully loaded */
+var domReady = function(callback) {
+    'use strict';
+    if (document.readyState === 'interactive' || document.readyState === 'complete') {
+        callback();
+    } else {
+        document.addEventListener('DOMContentLoaded', callback);
+    }
+};
+
 /**
  * Get python context and fill in js global variables with it
  */
 window.videoPlayerId = '{{ video_player_id }}';
 window.playerStateObj = JSON.parse('{{ player_state }}');
+
