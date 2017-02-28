935bd5df3e6469c6b0d3f91be176f80f4c6d89bb
xahgmah <xahgmah@yandex.ru>
2017-02-28 13:05:29 +0200

    Fix one second cycling
    Fix test

--- video_xblock/static/js/context.js
+++ video_xblock/static/js/context.js
@@ -1,13 +1,3 @@
-/** Run a callback when DOM is fully loaded */
-var domReady = function(callback) {
-    'use strict';
-    if (document.readyState === 'interactive' || document.readyState === 'complete') {
-        callback();
-    } else {
-        document.addEventListener('DOMContentLoaded', callback);
-    }
-};
-
 /**
  * Get python context and fill in js global variables with it
  */
