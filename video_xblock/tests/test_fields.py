7ab8978dd744306a3fc7adbe4bfbe52c6198fe59
TamoshaytisV <amd.vladas@gmail.com>
2017-03-02 13:31:10 +0200

    refactor RelativeTimeTest `delta` variable name

--- video_xblock/tests/test_fields.py
+++ video_xblock/tests/test_fields.py
@@ -12,47 +12,47 @@ class RelativeTimeTest(unittest.TestCase):
     """
     Test RelativeTime field.
     """
-    delta = RelativeTime()
+    timedelta = RelativeTime()
 
     def test_from_json(self):
         """
         Check conversion of data returned from json.
         """
         self.assertEqual(
-            RelativeTimeTest.delta.from_json('0:05:07'),
+            RelativeTimeTest.timedelta.from_json('0:05:07'),
             datetime.timedelta(seconds=307)
         )
 
         self.assertEqual(
-            RelativeTimeTest.delta.from_json(100.0),
+            RelativeTimeTest.timedelta.from_json(100.0),
             datetime.timedelta(seconds=100)
         )
         self.assertEqual(
-            RelativeTimeTest.delta.from_json(None),
+            RelativeTimeTest.timedelta.from_json(None),
             datetime.timedelta(seconds=0)
         )
 
         with self.assertRaises(TypeError):
-            RelativeTimeTest.delta.from_json(1234)  # int
+            RelativeTimeTest.timedelta.from_json(1234)  # int
 
         with self.assertRaises(ValueError):
-            RelativeTimeTest.delta.from_json("77:77:77")
+            RelativeTimeTest.timedelta.from_json("77:77:77")
 
     def test_enforce_type(self):
         """
         Check type validation.
         """
-        self.assertEqual(RelativeTimeTest.delta.enforce_type(None), None)
+        self.assertEqual(RelativeTimeTest.timedelta.enforce_type(None), None)
         self.assertEqual(
-            RelativeTimeTest.delta.enforce_type(datetime.timedelta(days=1, seconds=46799)),
+            RelativeTimeTest.timedelta.enforce_type(datetime.timedelta(days=1, seconds=46799)),
             datetime.timedelta(days=1, seconds=46799)
         )
         self.assertEqual(
-            RelativeTimeTest.delta.enforce_type('0:05:07'),
+            RelativeTimeTest.timedelta.enforce_type('0:05:07'),
             datetime.timedelta(seconds=307)
         )
         with self.assertRaises(TypeError):
-            RelativeTimeTest.delta.enforce_type([1])
+            RelativeTimeTest.timedelta.enforce_type([1])
 
     def test_to_json(self):
         """
@@ -60,23 +60,23 @@ class RelativeTimeTest(unittest.TestCase):
         """
         self.assertEqual(
             "01:02:03",
-            RelativeTimeTest.delta.to_json(datetime.timedelta(seconds=3723))
+            RelativeTimeTest.timedelta.to_json(datetime.timedelta(seconds=3723))
         )
         self.assertEqual(
             "00:00:00",
-            RelativeTimeTest.delta.to_json(None)
+            RelativeTimeTest.timedelta.to_json(None)
         )
         self.assertEqual(
             "00:01:40",
-            RelativeTimeTest.delta.to_json(100.0)
+            RelativeTimeTest.timedelta.to_json(100.0)
         )
 
         error_msg = "RelativeTime max value is 23:59:59=86400.0 seconds, but 90000.0 seconds is passed"
         with self.assertRaisesRegexp(ValueError, error_msg):
-            RelativeTimeTest.delta.to_json(datetime.timedelta(seconds=90000))
+            RelativeTimeTest.timedelta.to_json(datetime.timedelta(seconds=90000))
 
         with self.assertRaises(TypeError):
-            RelativeTimeTest.delta.to_json("123")
+            RelativeTimeTest.timedelta.to_json("123")
 
     def test_str(self):
         """
@@ -84,9 +84,9 @@ class RelativeTimeTest(unittest.TestCase):
         """
         self.assertEqual(
             "01:02:03",
-            RelativeTimeTest.delta.to_json(datetime.timedelta(seconds=3723))
+            RelativeTimeTest.timedelta.to_json(datetime.timedelta(seconds=3723))
         )
         self.assertEqual(
             "11:02:03",
-            RelativeTimeTest.delta.to_json(datetime.timedelta(seconds=39723))
+            RelativeTimeTest.timedelta.to_json(datetime.timedelta(seconds=39723))
         )
