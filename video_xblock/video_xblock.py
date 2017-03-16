399f5da760d166a50d361eba5d31f8d5e8ffa986
dorosh <dorosshh@gmail.com>
2017-03-16 13:31:46 +0200

    Add 3playmedia service support
    - add the method for uploading vtt data
    - add the method for convert not valid vtt
    - add template studio-edit-playmedia_apikey
    - add js function getTranscripts3playmediaApi for connections with server and add event for call this function

--- video_xblock/video_xblock.py
+++ video_xblock/video_xblock.py
@@ -102,6 +102,181 @@ class TranscriptsMixin(XBlock):
                 )
             yield tran
 
+    def get_transcript_download_link(self):
+        """
+        Return link for downloading of a transcript of the current captions' language (if a transcript exists).
+        """
+        transcripts = json.loads(self.transcripts) if self.transcripts else []
+        for transcript in transcripts:
+            if transcript.get('lang') == self.captions_language:
+                return transcript.get('url')
+        return ''
+
+    def create_transcript_file(self, ext='.vtt', trans_str='', reference_name=''):
+        """
+        Upload a transcript, fetched from a video platform's API, to video xblock.
+
+        Arguments:
+            ext (str): format of transcript file, default is vtt.
+            trans_str (str): multiple string for convert to vtt file.
+            reference_name (str): name of transcript file.
+        Returns:
+            File's file_name and external_url.
+        """
+        # Define location of default transcript as a future asset and prepare content to store in assets
+        file_name = reference_name.replace(" ", "_") + ext
+        course_key = self.location.course_key  # pylint: disable=no-member
+        content_loc = self.static_content.compute_location(course_key, file_name)  # AssetLocator object
+        content = self.static_content(
+            content_loc,
+            file_name,
+            'application/json',
+            trans_str.encode('UTF-8')
+        )  # StaticContent object
+        external_url = '/' + str(content_loc)
+
+        # Commit the content
+        self.contentstore().save(content)
+
+        return file_name, external_url
+
+    def convert_3playmedia_caps_to_vtt(self, caps, video_id, lang="en", lang_label="English"):
+        """
+        Utility method to convert any supported transcripts into WebVTT format.
+
+        Arguments:
+            caps (unicode)  : Raw transcripts.
+            video_id (str)  : Video id from player.
+            lang (str)      : Iso code for language.
+            lang_label (str): Name of language.
+        Returns:
+            response (dict) : {"lang": lang, "url": url, "label": lang_label}
+                lang (str)  : Iso code for language.
+                url (str)   : External url for vtt file.
+                label (str) : Name of language.
+        """
+        out, response = [], {}
+        for item in caps.splitlines():
+            if item == '':
+                item = ' \n'
+            elif '-->' in item:
+                # This line is deltatime stamp 00:05:55.030 --> 00:05:57.200.
+                # Length this line is 29 characters.
+                item = item[:29]
+            out.append(item)
+
+        caps = u'\n'.join(out).replace('\n&nbsp;', '')
+        sub = self.convert_caps_to_vtt(caps=caps)
+        reference_name = "{lang_label}_captions_video_{video_id}".format(
+            lang_label=lang_label, video_id=video_id
+        ).encode('utf8')
+        file_name, external_url = self.create_transcript_file(
+            trans_str=sub, reference_name=reference_name
+        )
+        if file_name:
+            response = {"lang": lang, "url": external_url, "label": lang_label}
+        return response
+
+    def get_translations_from_3playmedia(self, file_id, apikey):
+        """
+        Method to fetched from 3playmedia translations for file_id.
+
+        Arguments:
+            file_id (str) : File id on 3playmedia.
+            apikey (str)  : Authentication key on 3playmedia.
+        Returns:
+            response (tuple)    : status, translations or status, error_message
+            status (str)        : Status response error or success.
+            translations (list) : List of translations (dict) .
+            error_message (dict): Description of error.
+        """
+        domain = 'https://static.3playmedia.com/'
+        transcripts_3playmedia = requests.get(
+            '{domain}files/{file_id}/translations?apikey={api_key}'.format(
+                domain=domain, file_id=file_id, api_key=apikey
+            )
+        ).json()
+        errors = isinstance(transcripts_3playmedia, dict) and transcripts_3playmedia.get('errors')
+        if errors:
+            return 'error', {'error_message': u'\n'.join(errors.values())}
+
+        translations = []
+        for transcript in transcripts_3playmedia:
+            tid = transcript.get('id', '')
+            sub_unicode = requests.get(
+                '{domain}files/{file_id}/translations/{tid}/captions.vtt?apikey={api_key}'.format(
+                    domain=domain, file_id=file_id, api_key=apikey, tid=tid
+                )
+            ).text
+            translations.append(
+                self.convert_3playmedia_caps_to_vtt(
+                    caps=sub_unicode,
+                    video_id=self.get_player().media_id(self.href),
+                    lang=transcript.get('target_language_iso_639_1_code', ''),
+                    lang_label=transcript.get('target_language_name', '')
+                )
+            )
+        return 'success', translations
+
+    @XBlock.json_handler
+    def get_transcripts_3playmedia_api_handler(self, data, suffix=''):  # pylint: disable=unused-argument
+        """
+        Xblock handler to authenticate to a video platform's API. Called by JavaScript of `studio_view`.
+
+        Arguments:
+            data (dict): Data from frontend, necessary for authentication (tokens, account id, etc).
+            suffix (str): Slug used for routing.
+        Returns:
+            response (dict): Status messages key-value pairs.
+        """
+        apikey = data.get('api_key', self.threeplaymedia_apikey) or ''
+        file_id = data.get('file_id', '')
+        status, _transcripts = self.get_translations_from_3playmedia(
+            apikey=apikey, file_id=file_id
+        )
+        if status == 'error':
+            return _transcripts
+
+        transcript_original = requests.get(
+            'https://static.3playmedia.com/files/{file_id}/transcript.vtt?apikey={api_key}'.format(
+                file_id=file_id, api_key=apikey
+            )
+        ).text
+        _transcripts.append(
+            self.convert_3playmedia_caps_to_vtt(
+                caps=transcript_original,
+                video_id=self.get_player().media_id(self.href)
+            )
+        )
+        return {
+            'transcripts': _transcripts,
+            'success_message': _(
+                'Successfully fetched transcripts from 3playMedia. Please check transcripts list above.'
+            )
+        }
+
+    @XBlock.handler
+    def download_transcript(self, request, suffix=''):  # pylint: disable=unused-argument
+        """
+        Download a transcript.
+
+        Arguments:
+            request (webob.Request): Request to handle.
+            suffix (string): Slug used for routing.
+        Returns:
+            File with the correct name.
+        """
+        trans_path = self.get_path_for(request.query_string)
+        result = requests.get(request.host_url + request.query_string).text
+        filename = self.get_file_name_from_path(trans_path)
+        response = Response(result)
+        headerlist = [
+            ('Content-Type', 'text/plain'),
+            ('Content-Disposition', 'attachment; filename={}'.format(filename))
+        ]
+        response.headerlist = headerlist
+        return response
+
     @XBlock.handler
     def srt_to_vtt(self, request, suffix=''):  # pylint: disable=unused-argument
         """
@@ -809,144 +984,6 @@ class VideoXBlock(TranscriptsMixin, PlaybackStateMixin, StudioEditableXBlockMixi
             return os.path.join('/', file_field)
         return ''
 
-    def get_transcript_download_link(self):
-        """
-        Return link for downloading of a transcript of the current captions' language (if a transcript exists).
-        """
-        transcripts = json.loads(self.transcripts) if self.transcripts else []
-        for transcript in transcripts:
-            if transcript.get('lang') == self.captions_language:
-                return transcript.get('url')
-        return ''
-
-    def create_transcript_file(self, ext='.vtt', trans_str='', reference_name=''):
-        """
-        Upload a transcript, fetched from a video platform's API, to video xblock.
-
-        Arguments:
-            ext (str): format of transcript file, default is vtt.
-            trans_str (str): multiple string for convert to vtt file.
-            reference_name (str): name of transcript file.
-        Returns:
-            File's file_name and external_url.
-        """
-        # Define location of default transcript as a future asset and prepare content to store in assets
-        file_name = reference_name.replace(" ", "_") + ext
-        course_key = self.location.course_key  # pylint: disable=no-member
-        content_loc = self.static_content.compute_location(course_key, file_name)  # AssetLocator object
-        content = self.static_content(
-            content_loc,
-            file_name,
-            'application/json',
-            trans_str.encode('UTF-8')
-        )  # StaticContent object
-        external_url = '/' + str(content_loc)
-
-        # Commit the content
-        self.contentstore().save(content)
-
-        return file_name, external_url
-
-    def convert_3playmedia_caps_to_vtt(self, caps, video_id, lang="en", lang_label="English"):
-        """
-        Utility method to convert any supported transcripts into WebVTT format.
-
-        Arguments:
-            caps (unicode)  : Raw transcripts.
-            video_id (str)  : Video id from player.
-            lang (str)      : Iso code for language.
-            lang_label (str): Name of language.
-        Returns:
-            response (dict) : {"lang": lang, "url": url, "label": lang_label}
-                lang (str)  : Iso code for language.
-                url (str)   : External url for vtt file.
-                label (str) : Name of language.
-        """
-        out, response = [], {}
-        for item in caps.splitlines():
-            if item == '':
-                item = ' \n'
-            elif '-->' in item:
-                # This line is deltatime stamp 00:05:55.030 --> 00:05:57.200.
-                # Length this line is 29 characters.
-                item = item[:29]
-            out.append(item)
-
-        caps = u'\n'.join(out).replace('\n&nbsp;', '')
-        sub = self.convert_caps_to_vtt(caps=caps)
-        reference_name = "{lang_label}_captions_video_{video_id}".format(
-            lang_label=lang_label, video_id=video_id
-        ).encode('utf8')
-        file_name, external_url = self.create_transcript_file(
-            trans_str=sub, reference_name=reference_name
-        )
-        if file_name:
-            response = {"lang": lang, "url": external_url, "label": lang_label}
-        return response
-
-    def get_translations_from_3playmedia(self, file_id, apikey):
-        """
-        Method to fetched from 3playmedia translations for file_id.
-
-        Arguments:
-            file_id (str) : File id on 3playmedia.
-            apikey (str)  : Authentication key on 3playmedia.
-        Returns:
-            response (tuple)    : status, translations or status, error_message
-            status (str)        : Status response error or success.
-            translations (list) : List of translations (dict) .
-            error_message (dict): Description of error.
-        """
-        domain = 'https://static.3playmedia.com/'
-        transcripts_3playmedia = requests.get(
-            '{domain}files/{file_id}/translations?apikey={api_key}'.format(
-                domain=domain, file_id=file_id, api_key=apikey
-            )
-        ).json()
-        errors = isinstance(transcripts_3playmedia, dict) and transcripts_3playmedia.get('errors')
-        if errors:
-            return 'error', {'error_message': u'\n'.join(errors.values())}
-
-        translations = []
-        for transcript in transcripts_3playmedia:
-            tid = transcript.get('id', '')
-            sub_unicode = requests.get(
-                '{domain}files/{file_id}/translations/{tid}/captions.vtt?apikey={api_key}'.format(
-                    domain=domain, file_id=file_id, api_key=apikey, tid=tid
-                )
-            ).text
-            translations.append(
-                self.convert_3playmedia_caps_to_vtt(
-                    caps=sub_unicode,
-                    video_id=self.get_player().media_id(self.href),
-                    lang=transcript.get('target_language_iso_639_1_code', ''),
-                    lang_label=transcript.get('target_language_name', '')
-                )
-            )
-        return 'success', translations
-
-    @XBlock.handler
-    def download_transcript(self, request, suffix=''):  # pylint: disable=unused-argument
-        """
-        Download a transcript.
-
-        Arguments:
-            request (webob.Request): Request to handle.
-            suffix (string): Slug used for routing.
-        Returns:
-            File with the correct name.
-        """
-        trans_path = self.get_path_for(request.query_string)
-        result = requests.get(request.host_url + request.query_string).text
-        filename = self.get_file_name_from_path(trans_path)
-        response = Response(result)
-        headerlist = [
-            ('Content-Type', 'text/plain'),
-            ('Content-Disposition', 'attachment; filename={}'.format(filename))
-        ]
-        response.headerlist = headerlist
-        return response
-
     @XBlock.json_handler
     def dispatch(self, request, suffix):
         """
@@ -1034,43 +1071,6 @@ class VideoXBlock(TranscriptsMixin, PlaybackStateMixin, StudioEditableXBlockMixi
         # Metadata is to be updated on each authentication effort.
         self.update_metadata_authentication(auth_data=auth_data, player=player)
         return auth_data, error_message
-
-    @XBlock.json_handler
-    def get_transcripts_3playmedia_api_handler(self, data, suffix=''):  # pylint: disable=unused-argument
-        """
-        Xblock handler to authenticate to a video platform's API. Called by JavaScript of `studio_view`.
-
-        Arguments:
-            data (dict): Data from frontend, necessary for authentication (tokens, account id, etc).
-            suffix (str): Slug used for routing.
-        Returns:
-            response (dict): Status messages key-value pairs.
-        """
-        apikey = data.get('api_key', self.threeplaymedia_apikey) or ''
-        file_id = data.get('file_id', '')
-        status, _transcripts = self.get_translations_from_3playmedia(
-            apikey=apikey, file_id=file_id
-        )
-        if status == 'error':
-            return _transcripts
-
-        transcript_original = requests.get(
-            'https://static.3playmedia.com/files/{file_id}/transcript.vtt?apikey={api_key}'.format(
-                file_id=file_id, api_key=apikey
-            )
-        ).text
-        _transcripts.append(
-            self.convert_3playmedia_caps_to_vtt(
-                caps=transcript_original,
-                video_id=self.get_player().media_id(self.href)
-            )
-        )
-        return {
-            'transcripts': _transcripts,
-            'success_message': _(
-                'Successfully fetched transcripts from 3playMedia. Please check transcripts list above.'
-            )
-        }
 
     @XBlock.json_handler
     def authenticate_video_api_handler(self, data, suffix=''):  # pylint: disable=unused-argument
