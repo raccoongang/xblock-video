6a9054282fe3b892ee9d7014ca356dc9228ece47
xahgmah <xahgmah@yandex.ru>
2017-03-23 18:34:14 +0200

    add tests for getTranscrpitUrl and validateTranscripts

--- video_xblock/static/js/spec/studio-edit/transcript_manual_upload_spec.js
+++ video_xblock/static/js/spec/studio-edit/transcript_manual_upload_spec.js
@@ -1,9 +1,9 @@
-/* global parseRelativeTime */
+/* global parseRelativeTime validateTranscripts*/
 /**
  * Tests for transcripts manual upload
  */
 
-describe('Studio edit utils', function() {
+describe('Transcripts manual upload', function() {
     'use strict';
     it('return parseRelativeTime', function() {
         var tests = {
@@ -20,5 +20,39 @@ describe('Studio edit utils', function() {
         Object.keys(tests).forEach(function(value) {
             expect(parseRelativeTime(value)).toBe(tests[value]);
         });
+    });
+    it('return getTranscriptUrl', function() {
+        var transcriptsArray = [
+            {
+                lang: 'en',
+                url: 'http://test.en/'
+            },
+            {
+                lang: 'ru',
+                url: 'http://test.ru/'
+            }
+        ];
+        expect(getTranscriptUrl(transcriptsArray, 'en')).toBe('http://test.en/');
+        expect(getTranscriptUrl(transcriptsArray, 'ru')).toBe('http://test.ru/');
+        expect(getTranscriptUrl(transcriptsArray)).toBe('');
+    });
+
+    it('return validateTranscripts', function() {
+        var $testTranscriptsBlock;
+        var e = {
+            preventDefault: function() {}
+        };
+        $('body').append('<ol id="test-transcript-block" class="list-settings language-transcript-selector">' +
+            '<li class="list-settings-item">' +
+            '<div class="list-settings-buttons">' +
+            '<a href="#" class="download-transcript download-setting is-hidden">Download</a>' +
+            '</div>' +
+            '</li>' +
+            '</ol>');
+
+        $testTranscriptsBlock = $('#test-transcript-block');
+        expect(validateTranscripts(e, $testTranscriptsBlock)).toBeFalsy();
+        $testTranscriptsBlock.first('li').find('.download-setting').removeClass('is-hidden');
+        expect(validateTranscripts(e, $testTranscriptsBlock)).toBeTruthy();
     });
 });
