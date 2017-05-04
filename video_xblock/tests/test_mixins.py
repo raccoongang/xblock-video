6e324c5a68adc3ce54895971b4534cf3d692b0d3
Alexander Zayats <alexander.zayats@gmail.com>
2017-05-04 17:25:54 +0300

    Refactor mixin tests

--- video_xblock/tests/test_mixins.py
+++ video_xblock/tests/test_mixins.py
@@ -71,11 +71,13 @@ class LocationMixinTests(VideoXBlockTestBase):
         self.assertEqual(self.xblock.deprecated_string, 'deprecated_string')
 
     def setup_location_mock(self):
-        self.xblock.location = self.location_mock = Mock()
+        """Utility method to mock self.xblock.location"""
+        self.xblock.location = Mock()
 
     def test_block_id(self):
         self.setup_location_mock()
-        type(self.location_mock).block_id = block_mock = PropertyMock(return_value='test_block_id')
+        type(self.xblock.location).block_id = block_mock = \
+            PropertyMock(return_value='test_block_id')
 
         self.assertEqual(self.xblock.block_id, 'test_block_id')
         block_mock.assert_called_once()
@@ -83,7 +85,7 @@ class LocationMixinTests(VideoXBlockTestBase):
     def test_course_key(self):
         self.setup_location_mock()
         course_key_mock = \
-            type(self.location_mock).course_key = \
+            type(self.xblock.location).course_key = \
             PropertyMock(return_value='test_course_key')
 
         self.assertEqual(self.xblock.course_key, 'test_course_key')
@@ -91,7 +93,7 @@ class LocationMixinTests(VideoXBlockTestBase):
 
     def test_deprecated_string(self):
         self.setup_location_mock()
-        self.location_mock.to_deprecated_string = str_mock = Mock(return_value='test_str')
+        self.xblock.location.to_deprecated_string = str_mock = Mock(return_value='test_str')
 
         self.assertEqual(self.xblock.deprecated_string, 'test_str')
         str_mock.assert_called_once()
