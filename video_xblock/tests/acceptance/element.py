3b0f1ffe58f2b0d5f74c449efb635edbbbf157e3
Alexander Zayats <alexander.zayats@gmail.com>
2017-05-16 19:38:11 +0300

    Remove unused code

--- video_xblock/tests/acceptance/element.py
+++ video_xblock/tests/acceptance/element.py
@@ -1,5 +1,7 @@
 """
 Page elements to use with page objects.
+
+Reference: https://selenium-python.readthedocs.io/page-objects.html#page-elements
 """
 
 from selenium.webdriver.support.ui import WebDriverWait
@@ -35,30 +37,3 @@ class ClassPageElement(object):
             lambda driver: driver.find_element_by_class_name(self.locator))  # pylint: disable=no-member
         element = driver.find_element_by_class_name(self.locator)  # pylint: disable=no-member
         return element
-
-
-class InputPageElement(object):
-    """
-    Input page element.
-
-    Locates element by its name, can get and set element's text value.
-    """
-
-    def __set__(self, obj, value):
-        """
-        Set the text to the value supplied.
-        """
-        driver = obj.driver
-        WebDriverWait(driver, 100).until(
-            lambda driver: driver.find_element_by_name(self.locator))  # pylint: disable=no-member
-        driver.find_element_by_name(self.locator).send_keys(value)  # pylint: disable=no-member
-
-    def __get__(self, obj, owner):
-        """
-        Get the text value of the specified object.
-        """
-        driver = obj.driver
-        WebDriverWait(driver, 100).until(
-            lambda driver: driver.find_element_by_name(self.locator))  # pylint: disable=no-member
-        element = driver.find_element_by_name(self.locator)  # pylint: disable=no-member
-        return element.get_attribute('value')
