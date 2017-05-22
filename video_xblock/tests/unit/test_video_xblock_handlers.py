5eaad88a9fe85efdf276a30b35242cc863022452
Alexander Zayats <alexander.zayats@gmail.com>
2017-05-23 01:23:48 +0300

    Add docstrings

--- video_xblock/tests/unit/test_video_xblock_handlers.py
+++ video_xblock/tests/unit/test_video_xblock_handlers.py
@@ -1,3 +1,7 @@
+"""
+Test cases for VideoXBlock handlers
+"""
+
 import json
 from mock import patch, Mock
 
@@ -5,14 +9,25 @@ from video_xblock import VideoXBlock
 from video_xblock.tests.unit.base import VideoXBlockTestBase
 
 
+def arrange_request_mock(request_body):
+    """
+    Helper factory to create request mocks
+    """
+    request_mock = Mock()
+    request_mock.method = 'POST'
+    request_mock.body = request_body
+    return request_mock
+
+
 class AuthenticateApiHandlerTests(VideoXBlockTestBase):
+    """
+    Test cases for `VideoXBlock.authenticate_video_api_handler`
+    """
 
     @patch.object(VideoXBlock, 'authenticate_video_api')
     def test_auth_video_api_handler_delegates_call(self, auth_video_api_mock):
         # Arrange
-        request_mock = Mock()
-        request_mock.method = 'POST'
-        request_mock.body = '"test-token-123"'  # JSON string
+        request_mock = arrange_request_mock('"test-token-123"')  # JSON string
         auth_video_api_mock.return_value = {}, ''
 
         # Act
