5fcad50b3af0ff6bd1e712e50197174c50760b43
wowkalucky <wowkalucky@gmail.com>
2017-06-16 15:43:08 +0300

    Cover with unit tests "video_xblock/constants"

---
+++ video_xblock/tests/unit/test_constants.py
@@ -0,0 +1,45 @@
+"""
+Test cases for constants entities.
+"""
+
+import unittest
+
+from ddt import ddt, data
+from video_xblock.constants import TPMApiLanguage
+
+
+@ddt
+class ConstantsTest(unittest.TestCase):
+    """
+    Test Constants.
+    """
+
+    def test_lang_info(self):
+        """Test 3PlayMedia available transcript language_info object creation"""
+        self.assertRaises(ValueError, TPMApiLanguage('a'))
+        self.assertRaises(ValueError, TPMApiLanguage(999))
+
+    @data({
+        1: {
+            "ietf_code": "en",
+            "iso_639_1_code": "en",
+            "name": "English",
+            "full_name": "English",
+            "description": "All English variants"
+        },
+        48: {
+            "ietf_code": "",
+            "iso_639_1_code": "uk",
+            "name": "Ukrainian",
+            "full_name": "Ukrainian",
+            "description": ""
+        }
+    })
+    def test_lang_info(self, test_data):
+        """Test 3PlayMedia available transcript language_info object creation"""
+        for lang_id, lang_info in test_data.items():
+            self.assertEqual(TPMApiLanguage(lang_id).ietf_code, lang_info["ietf_code"])
+            self.assertEqual(TPMApiLanguage(lang_id).iso_639_1_code, lang_info["iso_639_1_code"])
+            self.assertEqual(TPMApiLanguage(lang_id).name, lang_info["name"])
+            self.assertEqual(TPMApiLanguage(lang_id).full_name, lang_info["full_name"])
+            self.assertEqual(TPMApiLanguage(lang_id).description, lang_info["description"])
